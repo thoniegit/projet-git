c8e893e4a99ce7026e2f67d16655db7fc6336b92
thoniegit <125136406+thoniegit@users.noreply.github.com>
2023-02-11 20:32:49 +0300

    troisième commit

--- src/helloWworld.java
+++ src/helloWworld.java
@@ -1,6 +1,7 @@
 public class helloWworld {
     public static void main(String[] args) {
-        System.out.println("Salut  les gens");
-        System.out.println("Samut samson");
+       Patient p= new Patient();
+       p.setNom("Samson");
+       System.out.println(p.getNom());
     }
 }
